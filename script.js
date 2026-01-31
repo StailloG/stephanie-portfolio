4fc04aac95dfac489ef9216b1ef8277e2a85602b
Stephanie Castillo <stephie.castles@gmail.com>
2026-01-31 00:51:14 -0500

    added typewriting in intro

--- script.js
+++ script.js
@@ -22,3 +22,59 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
             })
         });
+
+class Typewriter {
+            constructor(el, options){
+                this.el = el;
+                this.words = [...  this.el.dataset.typewriter.split(',')];
+                this.speed = options?.speed || 150;
+                this.delay = options?.delay || 1500;
+                this.repeat = options?.repeat || 150;
+                this.initTyping();
+            }
+
+            wait = (ms) => new Promise((resolve) =>
+            setTimeout(resolve, ms))
+
+            toggleTyping = () => this.el.classList.toggle('typing');
+
+            async typewrite(word) {
+                await this.wait(this.delay);
+                this.toggleTyping();
+                //loop through each letter
+                for (const letter of word.split('')) { 
+                    this.el.textContent += letter;
+                    await this.wait(this.speed)
+                }
+                this.toggleTyping();
+                await this.wait(this.delay);
+                //remove letters
+                this.toggleTyping();
+                while (this.el.textContent.length !== 0) {
+                    this.el.textContent = this.el.textContent.
+                    slice(0, -1);
+                    await this.wait(this.speed);
+                }
+                this.toggleTyping();
+            }
+
+            async initTyping() {
+                for (const word of this.words) {
+                    await this.typewrite(word);
+                }
+                if(this.repeat) {
+                    await this.initTyping();
+                }
+                else
+                {
+                    this.el.style.animation = 'none';
+                }
+            }
+        }
+
+        const el1 = new Typewriter(document.querySelector
+        ('[data-typewriter]'), {
+            speed: 100,
+            repeat: true,
+        });
+        console.log(el1);
